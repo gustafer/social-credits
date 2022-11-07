e95b313da8a96e4f5b0b9bebb91bb44ab1ec67ee
gustafer <gustavobreasy@gmail.com>
2022-11-06 21:52:23 -0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,91 @@
 import { useState } from 'react'
 import './App.scss'
 import { Text, Button, Grid } from '@mantine/core';
+import { atom, useRecoilState } from 'recoil';
 
 function App() {
-  let [credits, setCredits,] = useState(0);
+  const [credits, setCredits] = useState<number>(0);
+ 
+
+  const isChinaLoverState = atom({
+    key: 'isChinaLover', // unique ID (with respect to other atoms/selectors)
+    default: false, // default value (aka initial value)
+  });
 
 
-  if (credits > 999) {
-    let presidentChina = <h1>You are now the President of China!</h1>;
-    let xiJinping = <h3>Xi Jinping has successfully been demoted.</h3>
-    return (
-      <div>
-        {presidentChina} {xiJinping}
-        <Button onClick={() => setCredits((credits) => credits - 1000)}>I dont want</Button>
-      </div>
-    )
+  const isTaiwanLoverState = atom({
+    key: 'isTaiwanLover', // unique ID (with respect to other atoms/selectors)
+    default: false, // default value (aka initial value)
+  });
+
+  const [isChinaLover, setIsChinaLover] = useRecoilState(isChinaLoverState);
+  const [isTaiwanLover, setIsTaiwanLover] = useRecoilState(isTaiwanLoverState);
+  const showButtons = !isChinaLover && !isTaiwanLover;
+
+  const addCredits = () => {
+    const newCredit = credits + 100;
+    setCredits(newCredit);
+    if (newCredit > 999)
+      setIsChinaLover(true);
   }
 
-  if (credits < -999) {
-    let presidentTaiwan = <h1>You are now the President of Taiwan!</h1>;
-    let tsaiIngWen = <h3>Tsai Ing Wen has successfully been demoted.</h3>
-    return (
-      <div>
-        {presidentTaiwan} {tsaiIngWen}
-        <Button onClick={() => setCredits((credits) => credits + 1000)}>I dont want</Button>
-      </div>
-    )
+  const decreaseCredits = () => {
+    const newCredit = credits - 100;
+    setCredits(newCredit);
+    if (newCredit < -999)
+      setIsTaiwanLover(true);
+  }
+  
+  const clearCredits = () => {
+    setCredits(0);
+    setIsTaiwanLover(false);
+    setIsChinaLover(false);
   }
 
   return (
-    <><Text>quick test :</Text><div className="App">
-      <div>
-      </div>
-      <h1>Does taiwan make part of china?</h1>
-      <div>
+    <>
+      <div className="App">
+        {showButtons ? <>
+          <Text>quick test :</Text>
 
-
-        <div className="buttons">
-          <Grid columns={24} justify="center">
-            <Grid.Col span={12}>
-              <Button onClick={() => setCredits((credits) => credits + 100)}>
-                Yes
-              </Button>
-            </Grid.Col>
-            <Grid.Col span={12}>
-              <Button onClick={() => setCredits((credits) => credits - 100)}>
-                No
-              </Button>
-            </Grid.Col>
-          </Grid>
+          <h1>Does taiwan make part of china?</h1>
+        </> : <></>
+        }
+        <div>
+          {isChinaLover ?
+            <div>
+              <h1>You are now the President of China!</h1> <h3>Xi Jinping has successfully been demoted.</h3>
+              <Button onClick={clearCredits}>I dont want</Button>
+            </div>
+            : <></>
+          }
+          {isTaiwanLover ?
+            <div>
+              <h1>You are now the President of Taiwan!</h1> <h3>Tsai Ing Wen has successfully been demoted.</h3>
+              <Button onClick={clearCredits}>I dont want</Button>
+            </div>
+            : <></>
+          }
+          {showButtons ? <>
+            <div className="buttons">
+              <Grid columns={24} justify="center">
+                <Grid.Col span={12}>
+                  <Button onClick={addCredits}>
+                    Yes
+                  </Button>
+                </Grid.Col>
+                <Grid.Col span={12}>
+                  <Button onClick={decreaseCredits}>
+                    No
+                  </Button>
+                </Grid.Col>
+              </Grid>
+            </div>
+            <h1>{credits}</h1>
+            <Text>social credits.</Text>
+          </> : <></>}
         </div>
-        <h1>{credits}</h1>
-
-        <Text>social credits.</Text>
-      </div>
-    </div></>
+      </div></>
   )
 }
 export default App;
--- src/President.tsx
+++ src/President.tsx
@@ -1,19 +1,44 @@
 
-import { Space, Text } from '@mantine/core';
+import { Input, Space, Text } from '@mantine/core';
+import { useState } from 'react';
+import { atom, useRecoilValue } from 'recoil';
 
 
 function President() {
+    const [name, setName] = useState<string>('You')
+    const isChinaLoverState = atom({
+        key: 'isChinaLover', // unique ID (with respect to other atoms/selectors)
+        default: false, // default value (aka initial value)
+    });
+
+
+    const isTaiwanLoverState = atom({
+        key: 'isTaiwanLover', // unique ID (with respect to other atoms/selectors)
+        default: false, // default value (aka initial value)
+    });
+
+    const isChinaLover = useRecoilValue(isChinaLoverState);
+    const isTaiwanLover = useRecoilValue(isTaiwanLoverState);
+    const showInput = isChinaLover || isTaiwanLover;
+ 
     return (
         <>
-          <Space h="xl" />
-          <Space h="xl" />
-          <Space h="xl" />
+            <Space h="xl" />
+            <Space h="xl" />
+            <Space h="xl" />
+            {showInput ?
+                <Input defaultValue={name === 'You' ? '' : name} placeholder='your name here' onChange={(event: React.FormEvent<HTMLInputElement>) => setName(event.currentTarget.value)} /> :
+                <></>
+            }
+
+
             <div>
                 <Text >
                     <h3>
                         actual presidents :
                     </h3>
-                    <strong>china:</strong> Xi Jinping, <strong>taiwan:</strong> Tsai Ing Wen
+                    <strong>china: </strong>{isChinaLover ? name : 'Xi Jinping'}<br />
+                    <strong>taiwan: </strong>{isTaiwanLover ? name : 'Tsai Ing Wen'}
                 </Text >
             </div>
         </>
