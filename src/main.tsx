1c6b41bc95e8b31f746865f7842716c883df190d
gustafer <gustavobreasy@gmail.com>
2022-11-06 21:52:22 -0300

    add recoil and many improvements

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
 import { MantineProvider } from '@mantine/core'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { atom, RecoilRoot } from 'recoil'
 import App from './App'
 import President from './President'
+
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 
   <MantineProvider theme={{ colorScheme: 'dark' }} withGlobalStyles withNormalizeCSS>
-    <React.StrictMode>
-      <App />
-      <President />
-    </React.StrictMode>
+    <RecoilRoot>
+      <React.StrictMode>
+        <App />
+        <President />
+      </React.StrictMode>
+    </RecoilRoot>
   </MantineProvider>
 )
